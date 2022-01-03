f04ee0aa9adf4f6530f1282ed41b17afbbf78451
Thooyavan <mail@thooyavan.me>
2022-01-03 10:27:03 +0530

    bugfix: missing template mapping (#9)

--- layman_erp/settings.py
+++ layman_erp/settings.py
@@ -92,7 +92,10 @@ ROOT_URLCONF = 'layman_erp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'core/templates'),
+            os.path.join(BASE_DIR, 'templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
